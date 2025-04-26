03553c87a21c0c8bbe83734bdcfe26ef8dd950ba
MelihEmreGuler <gulermelihemre@gmail.com>
2025-04-26 14:21:41 +0300

    add WebConfig

---
+++ src/main/java/tr/edu/duzce/mf/bm/bm470captcha/config/WebConfig.java
@@ -0,0 +1,28 @@
+package tr.edu.duzce.mf.bm.bm470captcha.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"tr.edu.duzce.mf.bm.bm470captcha"})
+public class WebConfig implements WebMvcConfigurer {
+
+
+    @Bean
+    public InternalResourceViewResolver jspViewResolver() {
+        InternalResourceViewResolver viewResolver = new
+                InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/view/");
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setContentType("text/html;charset=UTF-8");
+        return viewResolver;
+    }
+}
+
