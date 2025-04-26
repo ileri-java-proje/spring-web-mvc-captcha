8d487ae59fec9469f66a56a9bada2e60b6ad00f7
Hasan ÖZCAN <hasan26has@gmail.com>
2025-04-26 14:33:42 +0300

    WebConfig_update

--- src/main/java/tr/edu/duzce/mf/bm/bm470captcha/config/WebConfig.java
+++ src/main/java/tr/edu/duzce/mf/bm/bm470captcha/config/WebConfig.java
@@ -1,12 +1,20 @@
 package tr.edu.duzce.mf.bm.bm470captcha.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
+import java.util.Locale;
 
 @Configuration
 @EnableWebMvc
@@ -24,5 +32,36 @@ public class WebConfig implements WebMvcConfigurer {
         viewResolver.setContentType("text/html;charset=UTF-8");
         return viewResolver;
     }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver clr = new CookieLocaleResolver();
+        clr.setDefaultLocale(Locale.forLanguageTag("tr-TR"));   // varsayılan
+        clr.setCookieName("bm470-captcha");               // çerez adı
+        clr.setCookieMaxAge(3600);                              // saniye
+        return clr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource ms = new ReloadableResourceBundleMessageSource();
+        ms.setBasename("classpath:messages");   // messages.properties kökü
+        ms.setDefaultEncoding("UTF-8");
+        ms.setFallbackToSystemLocale(false);    // sistem diline düşmesin
+        return ms;
+    }
+    
 }
 
